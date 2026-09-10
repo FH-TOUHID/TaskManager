8af1a243eae5d09e65e2c729f51c1fe1e246fe36
TOUHID <touhid192005@gmail.com>
2026-09-11 00:23:51 +0600

    Update CORS for Vercel frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,10 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://taskmanager-a4v52u6gw-touhid-s-projects.vercel.app",
+    ],
   })
 );
 app.use(express.json());
